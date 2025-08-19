46d2129ac04d1168b83c63bbfbe9a1b4f20f2d82
Ronaldo Carabali <48156273+desarrollosipcommb@users.noreply.github.com>
2025-08-19 10:30:20 -0500

    beta 0.6

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -15,7 +15,7 @@ export class Config {
     islogged: boolean = false;
     user: any;
 
-    version = 'V b0.2';
+    version = 'V b0.6';
     anio = '2025';
 
     constructor(public tokenService: TokenService) { }
