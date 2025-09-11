a1e86237b2be304e7af8a46e654ee93b663f7dc1
Ronaldo Carabali <48156273+desarrollosipcommb@users.noreply.github.com>
2025-09-11 16:45:35 -0500

    Correccion environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl:"https://rapimerqueback.sipcommbsas.app/api/v1/"
+    baseUrl:"https://todoenvasesback.sipcommbsas.app/"
     //baseUrl:"http://localhost:8080/api/v1/"
 };
