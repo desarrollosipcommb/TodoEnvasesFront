8a57aa8c082595d30562ea33c7d5036b33f590c0
Ronaldo Carabali <48156273+desarrollosipcommb@users.noreply.github.com>
2025-09-19 16:57:57 -0500

    Actualizar inventario

--- src/app/components/subir-excel/subir-excel.component.ts
+++ src/app/components/subir-excel/subir-excel.component.ts
@@ -3,6 +3,8 @@ import { SubirExcelService } from 'src/app/services/subir-excel.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { AlertaService } from 'src/app/services/alerta.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-subir-excel',
@@ -29,24 +31,70 @@ export class SubirExcelComponent {
 
 
 
-  constructor(private fileService: SubirExcelService) { }
+  constructor(private fileService: SubirExcelService,
+    private alerta: AlertaService
+  ) { }
 
+  /**
+   * Confirmar accion
+   * @param id identificardor del registro
+   * @param tipo Eliminar Activar
+   */
+  confirmar(file: Event, action: string): void {
+    Swal.fire({
+      title: '¿Esta seguro?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#a6b00e',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Confirmar!',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        if (action == "AGREGAR") {
+          this.onFileSelected(file)
+        } else if (action == "ACTUALIZAR") {
 
+        }
+
+      }
+    })
+  }
 
   onFileSelected(event: Event) {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length == 1 && this.revisarArchivo(input.files[0])) {
       const file = input.files[0];
 
-      console.log(file);
-      this.fileService.subirArchivo(file).subscribe(
+      this.fileService.agregarInventario(file).subscribe(
         response => {
-          console.log('Archivo subido exitosamente:', response);
+          this.alerta.success('Inventario añadido', '');
           this.resultados = Array.isArray(response) ? response : Object.values(response);
           this.organizarResultados(this.resultados);
         },
         error => {
-          console.error('Error al subir el archivo:', error);
+          this.alerta.error('Error al subir el archivo:', error)
+        }
+      );
+    } else {
+      alert("Por favor, selecciona solo un archivo del tipo .xls o .xlsx.");
+    }
+    input.value = ''; // Limpiar el input para permitir subir el mismo archivo nuevamente
+  }
+
+  actualizarInventario(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length == 1 && this.revisarArchivo(input.files[0])) {
+      const file = input.files[0];
+
+      this.fileService.actualizarInventario(file).subscribe(
+        response => {
+          this.alerta.success('Cantidades actualizadas', '');
+          this.resultados = Array.isArray(response) ? response : Object.values(response);
+          this.organizarResultados(this.resultados);
+        },
+        error => {
+          this.alerta.error('Error al subir el archivo:', error)
         }
       );
     } else {
@@ -56,14 +104,14 @@ export class SubirExcelComponent {
   }
 
   revisarArchivo(file: File) {
-   if (file.name.endsWith('.xlsx') || file.name.endsWith('.xls')) {
+    if (file.name.endsWith('.xlsx') || file.name.endsWith('.xls')) {
       // El archivo es un Excel válido
       return true;
     }
     return false;
   }
 
-   tipos: string[] = ['diametros', 'tapas', 'envases', 'quimicos', 'extracto'];
+  tipos: string[] = ['diametros', 'tapas', 'envases', 'quimicos', 'extracto'];
   tipoSeleccionado: string = 'diametros';
 
   resultadosPorTipo: { [key: string]: { fileName: string; message: string }[] } = {
@@ -106,31 +154,31 @@ export class SubirExcelComponent {
     if (this.paginator) this.paginator.firstPage();
   }
 
-pageSize: number = 10;
-currentPage: number = 1;
+  pageSize: number = 10;
+  currentPage: number = 1;
 
-getRowClass(element: { message: string }) {
-  const msg = element.message.toLowerCase();
-  if (msg.toLowerCase().includes('error') && !msg.includes('ya existe')) return 'row-error';
-  if (msg.includes('ya existe')) return 'row-warning';
-  if (msg.includes('agregado')) return 'row-success';
-  return '';
-}
+  getRowClass(element: { message: string }) {
+    const msg = element.message.toLowerCase();
+    if (msg.toLowerCase().includes('error') && !msg.includes('ya existe')) return 'row-error';
+    if (msg.includes('ya existe')) return 'row-warning';
+    if (msg.includes('agregado')) return 'row-success';
+    return '';
+  }
 
-get paginatedResultados() {
-  const lista = this.resultadosPorTipo[this.tipoSeleccionado] ?? [];
-  const start = (this.currentPage - 1) * this.pageSize;
-  return lista.slice(start, start + this.pageSize);
-}
+  get paginatedResultados() {
+    const lista = this.resultadosPorTipo[this.tipoSeleccionado] ?? [];
+    const start = (this.currentPage - 1) * this.pageSize;
+    return lista.slice(start, start + this.pageSize);
+  }
 
-get totalPages() {
-  const lista = this.resultadosPorTipo[this.tipoSeleccionado] ?? [];
-  return Math.ceil(lista.length / this.pageSize);
-}
+  get totalPages() {
+    const lista = this.resultadosPorTipo[this.tipoSeleccionado] ?? [];
+    return Math.ceil(lista.length / this.pageSize);
+  }
 
-changePage(page: number) {
-  this.currentPage = page;
-}
+  changePage(page: number) {
+    this.currentPage = page;
+  }
 
 
 
--- src/app/services/subir-excel.service.ts
+++ src/app/services/subir-excel.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -16,11 +16,23 @@ export class SubirExcelService {
    * @param file Archivo Excel
    * @returns Observable con la respuesta del backend
    */
-  public subirArchivo(file: File): Observable<any> {
+  public agregarInventario(file: File): Observable<any> {
     const formData = new FormData();
     formData.append('file', file);
 
-    // No se necesita Content-Type, Angular lo pone automáticamente para FormData
-    return this.httpCliente.post<any>(this.url+'upload-excel', formData);
+    return this.httpCliente.post<any>(this.url + 'upload-excel', formData);
+  }
+
+
+  /**
+   * Sube un archivo Excel al backend que actualiza solo las cantidades de los items ya existentes
+   * @param file Archivo Excel
+   * @returns Observable con la respuesta del backend
+   */
+  public actualizarInventario(file: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    return this.httpCliente.post<any>(this.url + 'update-inventory-excel', formData);
   }
 }
